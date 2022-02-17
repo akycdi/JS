5fd069c2469aaf2a6651e380ef6bc50336ded825
arunkumar36824 <arun36824@gmail.com>
2022-02-17 14:42:38 +0530

    testing github

---
+++ counterApp/index.js
@@ -0,0 +1,15 @@
+let count=0;
+function fun(){
+   document.getElementById("count-el").innerHTML =count;
+  count++;
+    if(count == 10){
+        alert("chalu bro emka")
+    }
+    if(count ==11 ){
+        alert("Bro enti bro nuvu ela unav")
+
+    }
+    if(count == 12 ){
+        alert("sarele bro chaala chusam")
+    }
+}
